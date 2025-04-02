3714f0ae2ab8ae45e1b6bd51def9e6a8bbd75814
RonSalvet <ronaldsalvatierraflores@gmail.com>
2025-04-01 20:54:26 -0400

    agregamos navbar y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
 
 export const routes: Routes = [
-    { path: "home", component: HomeComponent }
+    { path: "home", component: HomeComponent },
+    { path: "servicios", component: ServiciosComponent },
+    { path: "**", component: HomeComponent },
 ];
